263a1213d9331db4054d261be44ead7a3e576d30
parksk1213 <163983663+parksk1213@users.noreply.github.com>
2024-03-29 11:56:45 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,14 +3,21 @@
 # -- Project information
 
 project = 'FUSION'
-copyright = 'Fusion, ' \
-         'TingYuan, ' \
-         'Steven_Zhang, ' \
-         'FeiYu, ' \
-         'Zitai XuYang, ' \
-         'S.K.Park, ' \
-         'QuanCui, ' \
-         'Hanjin Liu, ' \
+copyright = 'Fusion, ' 
+
+         'TingYuan, ' 
+
+         'Steven_Zhang, ' 
+
+         'FeiYu, ' 
+         'Zitai XuYang, ' 
+
+         'S.K.Park, ' 
+
+         'QuanCui, ' 
+
+         'Hanjin Liu, ' 
+
          'Qianguang Zhao'
 author = 'Fusion'
 
